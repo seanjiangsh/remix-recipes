0100b3949d8f84d257d489fc707fcc9773b33c10
sean <seanjiangsh@gmail.com>
2024-03-26 10:23:48 +0800

    fix: fixed 'Route ID Collision' warning by updated remix.config.js to uses v1 route file convention

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,8 +1,24 @@
+import { LoaderFunction, json } from "@remix-run/node";
+import { PrismaClient, PantryShelf } from "@prisma/client";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async () => {
+  const db = new PrismaClient();
+  const shelves = await db.pantryShelf.findMany();
+  return json({ shelves });
+};
+
 export default function Pantry() {
+  const data = useLoaderData<{ shelves: Array<PantryShelf> }>();
+
   return (
     <div>
       <h1>Pantry</h1>
-      <p>Pantry page</p>
+      <ul>
+        {data.shelves.map(({ id, name }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- remix.config.js
+++ remix.config.js
@@ -2,13 +2,16 @@ import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/*.css"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
-
-  routes(definedRoutes) {
-    return createRoutesFromFolders(definedRoutes);
+  // Tell Remix to ignore everything in the routes directory.
+  // We'll let `createRoutesFromFolders` take care of that.
+  ignoredRouteFiles: ["**/*"],
+  routes: (defineRoutes) => {
+    // `createRoutesFromFolders` will create routes for all files in the
+    // routes directory using the same default conventions as Remix v1.
+    return createRoutesFromFolders(defineRoutes, {
+      // If you're already using `ignoredRouteFiles` in your Remix config,
+      // you can move them to `ignoredFilePatterns` in the plugin's options.
+      ignoredFilePatterns: ["**/.*", "**/*.css"],
+    });
   },
 };
