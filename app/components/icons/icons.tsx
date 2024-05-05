ec5ab6f30ca2567efd62b9d3ee92ddad6e0829a6
sean <seanjiangsh@gmail.com>
2024-05-05 09:03:13 +0800

    feat: implementing grocery list item component

--- app/components/icons/icons.tsx
+++ app/components/icons/icons.tsx
@@ -223,3 +223,20 @@ export const CloseIcon = () => (
     />
   </svg>
 );
+
+export const CheckIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-8 h-8"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+    />
+  </svg>
+);
--- app/routes/app/grocery-list.tsx
+++ app/routes/app/grocery-list.tsx
@@ -1,17 +1,38 @@
+import { useFetcher } from "@remix-run/react";
+
 import * as recipeTypes from "~/types/recipe/recipes";
+import { CheckIcon } from "~/components/icons/icons";
 
 const GroceryListItem = ({ item }: { item: recipeTypes.GroceryListItem }) => {
+  const { name, uses } = item;
+
+  const fetcher = useFetcher();
+  const fetcherIdle = fetcher.state === "idle";
+
   return (
-    <div>
-      <h3>{item.name}</h3>
-      <ul>
-        {item.uses.map((use) => (
-          <li key={use.id}>
-            {use.amount} {use.recipeName} x{use.multiplier}
-          </li>
-        ))}
-      </ul>
-    </div>
+    fetcherIdle && (
+      <div className="shadow-md rounded-md p-4 flex">
+        <div className="flex-grow">
+          <h1 className="text-sm font-bold mb-2 uppercase">{name}</h1>
+          <ul>
+            {uses.map(({ id, amount, recipeName, multiplier }) => (
+              <li key={id} className="py-1">
+                {amount} for {recipeName} (x{multiplier})
+              </li>
+            ))}
+          </ul>
+        </div>
+        <fetcher.Form method="post" className="flex flex-col justify-center">
+          <button
+            name="_action"
+            value="checkOffItem"
+            className="hover:text-primary"
+          >
+            <CheckIcon />
+          </button>
+        </fetcher.Form>
+      </div>
+    )
   );
 };
 
