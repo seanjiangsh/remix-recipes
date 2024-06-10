1f5d2abf366781f2f51266e073d90bca96c7f822
sean <seanjiangsh@gmail.com>
2024-06-10 09:43:06 +0800

    fix: trying to fix playwright workflow web server connection failed issue (15)

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm run dev > logs/server.log &",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
